499891ad59ff21e1e5972d66663eeb7c0573b7d9
profh <profh@cmu.edu>
2020-10-07 22:45:12 -0400

    Minor tweaks and comments

--- Repos/SearchRepositoriesClient.swift
+++ Repos/SearchRepositoriesClient.swift
@@ -4,9 +4,12 @@ import Alamofire
 class SearchRepositoriesClient {
   func fetchRepositories(_ completion: @escaping (Data?) -> Void) {
     
-    let urlString = "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"
+    let urlString = "https://api.github.com/search/repositories"
+    let params = ["q":"language:swift","sort":"stars","order":"desc"]
+    
+    // when combined in Alamofire, equivalent of  "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"
       
-    Alamofire.request(urlString).response { response in
+    Alamofire.request(urlString, parameters: params).response { response in
       if let error = response.error {
         print("Error fetching repositories: \(error)")
         completion(response.data)
